2aca8a39a7e12ec3e1126bd0df38741768ac89bc
humza_0237 <humzaali.bscssef20@iba-suk.edu.pk>
2024-02-08 22:25:11 +0500

    Added Dummy UI Code

--- app.py
+++ app.py
@@ -2,3 +2,20 @@ import gradio as gr
 from tensorflow.keras.models import load_model
 
 model = load_model('model.h5') # Remember to change model name to correct model name
+
+# Define a function to make predictions
+def predict_image(img):
+    # Preprocess the image
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+
+    # Make predictions
+    predictions = model.predict(img)
+    label = np.argmax(predictions)
+
+    return label
+
+# Create Gradio interface
+iface = gr.Interface(fn=predict_image, inputs="image", outputs="text")
+iface.launch()
